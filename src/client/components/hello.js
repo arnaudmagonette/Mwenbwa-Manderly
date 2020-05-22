c92cea6126726f74d592dadfb772089ff4fd2ffd
Arnaud Magonette <arnaud.magonette@gmail.com>
2020-05-22 09:53:38 +0200

    base du projet

---
+++ src/client/components/hello.js
@@ -0,0 +1,19 @@
+/* becodeorg/mwenbwa
+ *
+ * /src/client/components/hello.js - Hello Component
+ *
+ * coded by leny@BeCode
+ * started at 18/05/2020
+ */
+
+import * as React from "react";
+
+const HelloWorld = () => (
+    <div>
+        <h1>{"Hello, world!"}</h1>
+        <hr />
+        <small>{"becode/trouvkach"}</small>
+    </div>
+);
+
+export default HelloWorld;
