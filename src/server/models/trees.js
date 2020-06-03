2ad2192455178ba5202bba51c88a342f805d8151
Guillaume-Boeur <guillaume.boeur@gmail.com>
2020-06-03 14:57:03 +0200

    add db and connect to mongoose

---
+++ src/server/models/trees.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const treesSchema = new Schema(
+    {
+        name: {type: String, default: "For sale"},
+        leaves: Number,
+        sci_name: String,
+        geoloc: {
+            lat: Number,
+            lon: Number,
+        },
+        owner: [{type: String, default: "For sale"}],
+        comments: [{type: String, default: null}],
+        lock: {type: Boolean, default: false},
+    },
+    {collection: "trees"},
+);
+
+module.exports = mongoose.model("Trees", treesSchema);
