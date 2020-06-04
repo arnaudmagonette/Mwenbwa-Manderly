f9fae2e88718eda25cc9001ab1d5d185d91644bf
thomasdussart <thomas.dussart@gmail.com>
2020-06-04 13:24:18 +0200

    fix errors(eslint) react loginpage

--- src/client/components/login.js
+++ src/client/components/login.js
@@ -9,83 +9,80 @@ const LogIn = () => (
         <div>
             <div>
                 <h1 className={"has-text-centered is-size-1 has-margin-top-30"}>
-                    {"Welcome to Mwembwa"}{" "}
-                </h1>{" "}
-            </div>{" "}
+                    {"Welcome to Mwembwa"}
+                </h1>
+            </div>
             <div
                 className={
                     "container notification is-four-fifths has-margin-top-50"
                 }>
                 <div className={"columns is-size-3"}>
                     <div className={"column is-half has-padding-right-100"}>
-                        <div> {"Sign Up"} </div>{" "}
+                        <div> {"Sign Up"} </div>
                         <div className={"field"}>
                             {/* <label className="label has-padding-top-40">
-                                {" "}
-                                Name{" "}
-                            </label>{" "}
+                                
+                                Name
+                            </label>
                             <input
                                 className="input"
                                 type="text"
                                 placeholder="Type your name"
                             /> */}
                             <label className={"label has-padding-top-40"}>
-                                {" "}
-                                {"Username"}{" "}
-                            </label>{" "}
+                                {"Username"}
+                            </label>
                             <input
                                 className={"input is-success"}
                                 type={"email"}
                                 placeholder={"Type your username"}
                             />
                             <label className={"label has-padding-top-20"}>
-                                {" "}
-                                {"Email"}{" "}
-                            </label>{" "}
+                                {"Email"}
+                            </label>
                             <input
                                 className={"input"}
                                 type={"email"}
                                 placeholder={"Type your email"}
                             />
                             <label className={"label label has-padding-top-20"}>
-                                {"Password"}{" "}
-                            </label>{" "}
+                                {"Password"}
+                            </label>
                             <input
                                 className={"input is-success"}
                                 type={"password"}
                                 placeholder={"Type your password"}
                             />
                             <label className={"label has-padding-top-20"}>
-                                {"Confirm your password"}{" "}
-                            </label>{" "}
+                                {"Confirm your password"}
+                            </label>
                             <input
                                 className={"input is-success"}
                                 type={"Confirm your password"}
                                 placeholder={"Confirm your password"}
-                            />{" "}
+                            />
                             <div className={"control has-padding-top-40"}>
                                 <button
                                     className={"button is-primary is-medium"}>
                                     {"Submit"}
                                 </button>
                             </div>
-                        </div>{" "}
-                    </div>{" "}
+                        </div>
+                    </div>
                     <div className={"column is-half has-padding-right-100 "}>
-                        <div> {"Sign In"} </div>{" "}
+                        <div> {"Sign In"} </div>
                         <div className={"field "}>
                             <label className={"label has-padding-top-40"}>
-                                {" "}
-                                {"Username"}{" "}
-                            </label>{" "}
+                                {"Username"}
+                            </label>
                             <input
                                 className={"input is-success"}
                                 type={"email"}
                                 placeholder={"Type your username"}
                             />
                             <label className={"label label has-padding-top-20"}>
-                                {"Password"}{" "}
-                            </label>{" "}
+                                {"Password"}
+                            </label>
                             <input
                                 className={"input is-success"}
                                 type={"password"}
@@ -99,11 +96,11 @@ const LogIn = () => (
                                     {"Log in"}
                                 </button>
                             </div>
-                        </div>{" "}
-                    </div>{" "}
-                </div>{" "}
-            </div>{" "}
-        </div>{" "}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 );
 
