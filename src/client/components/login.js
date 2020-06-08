c6cd9c24b39286a73d865dfeb24abb324772b16a
thomasdussart <thomas.dussart@gmail.com>
2020-06-08 09:04:55 +0200

    login working

--- src/client/components/login.js
+++ src/client/components/login.js
@@ -34,7 +34,7 @@ export default class Login extends Component {
                     {
                         headers: response.data,
                     },
-                    window.location.replace("/"),
+                    //window.location.replace("/user/me"),
                 );
             })
             .catch(error => {
--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -145,7 +145,7 @@ router.get("/me", auth, async (req, res) => {
     try {
         // request.user is getting fetched from Middleware after token authentication
         const user = await User.findById(req.user.id);
-        res.json(user);
+        res.send(user);
     } catch (e) {
         res.json({message: "Error in Fetching user"});
     }
