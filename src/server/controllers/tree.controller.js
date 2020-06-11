7b80223ac742e856fab3dad7ac66e57f2bbfdf46
Guillaume-Boeur <guillaume.boeur@gmail.com>
2020-06-11 14:55:15 +0200

    change log.js to login.js

--- src/server/controllers/tree.controller.js
+++ src/server/controllers/tree.controller.js
@@ -3,7 +3,7 @@ const Tree = db.tree;
 
 exports.allTrees = (req, res) => {
     Tree.find({})
-        .limit(100)
+        .limit(1000)
         .exec((err, allTrees) => {
             if (err) {
                 res.status(500).send({message: err});
