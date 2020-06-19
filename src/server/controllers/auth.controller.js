dfa7a145f610858d60f18eed95c522746ad23018
thomasdussart <thomas.dussart@gmail.com>
2020-06-19 14:45:45 +0200

    heroku

--- src/server/controllers/auth.controller.js
+++ src/server/controllers/auth.controller.js
@@ -33,51 +33,43 @@ exports.signup = (req, res) => {
 exports.signin = (req, res) => {
     User.findOne({
         email: req.body.email,
-    })
-        .populate("roles", "-__v")
-        .exec((err, user) => {
-            if (err) {
-                res.status(500).send({message: err});
-                return;
-            }
+    }).exec((err, user) => {
+        if (err) {
+            res.status(500).send({message: err});
+            return;
+        }
 
-            if (!user) {
-                res.status(404).send({message: "User Not found."});
-                return;
-            }
+        if (!user) {
+            res.status(404).send({message: "User Not found."});
+            return;
+        }
 
-            const passwordIsValid = bcrypt.compareSync(
-                req.body.password,
-                user.password,
-            );
+        const passwordIsValid = bcrypt.compareSync(
+            req.body.password,
+            user.password,
+        );
 
-            if (!passwordIsValid) {
-                res.status(401).send({
-                    accessToken: null,
-                    message: "Invalid Password!",
-                });
-                return;
-            }
-
-            const token = jwt.sign({id: user.id}, config.secret, {
-                expiresIn: 86400, // 24 hours
+        if (!passwordIsValid) {
+            res.status(401).send({
+                accessToken: null,
+                message: "Invalid Password!",
             });
+            return;
+        }
 
-            const authorities = [];
-
-            for (let i = 0; i < user.roles.length; i++) {
-                authorities.push(`ROLE_${user.roles[i].name.toUpperCase()}`);
-            }
-            res.status(200).send({
-                id: user._id,
-                username: user.username,
-                email: user.email,
-                color: user.color,
-                leaves: user.leaves,
-                roles: authorities,
-                accessToken: token,
-            });
+        const token = jwt.sign({id: user.id}, config.secret, {
+            expiresIn: 86400, // 24 hours
         });
+
+        res.status(200).send({
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            color: user.color,
+            leaves: user.leaves,
+            accessToken: token,
+        });
+    });
 };
 
 // USER PROFILE
