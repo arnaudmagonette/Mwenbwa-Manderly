0f58a94b1a0fcb308d9d49db51b089dba81019e8
thomasdussart <thomas.dussart@gmail.com>
2020-06-19 11:05:58 +0200

    heroku

--- src/server/config/db.config.js
+++ src/server/config/db.config.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
 const mongoose = require("mongoose");
-const mongoURI = process.env.MONGOLAB_URI;
+// const mongoURI = process.env.MONGOLAB_URI;
 
 const ConnectionMongoDb = async () => {
     try {
-        await mongoose.connect(mongoURI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        });
+        await mongoose.connect(
+            "mongodb://heroku_2tcbx7hs:tq1onbmtutprqoq5ssb4mboqpi@ds249967.mlab.com:49967/heroku_2tcbx7hs",
+            {
+                useUnifiedTopology: true,
+                useNewUrlParser: true,
+            },
+        );
         console.log("Connected to DB !!");
     } catch (e) {
         console.log(e);
