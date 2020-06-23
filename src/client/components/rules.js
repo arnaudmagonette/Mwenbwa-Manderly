345e4e0316d729703930ec5e6b50c92fc46f76d5
Guillaume-Boeur <guillaume.boeur@gmail.com>
2020-06-23 12:12:49 +0200

    add icon in Rules

--- src/client/components/rules.js
+++ src/client/components/rules.js
@@ -1,5 +1,8 @@
 import React from "react";
 import AuthService from "../services/auth.service";
+import {Scrollbars} from "react-custom-scrollbars";
+import LeafIcon from "./leaf-icon";
+import TreeIcon from "./tree-icon";
 
 const connectedUser = AuthService.getCurrentUser();
 
@@ -11,11 +14,33 @@ const Rules = () => (
             }>
             {"RULES"}
         </p>
-        <p>{`Welcome to Mwenbwa ${connectedUser.username} !`}</p>
-        <p>{`Te voici voici à Liège, la cité ardente, et son étendue d'arbes. Tu viens de recevoir 3 arbres (tree icon) ainsi qu'une certaine somme de feuille (leaf icon) pour commencer ton expension. `}</p>
-        <p>{`Tu peux acheter des arbres libres (default tree icon) avec cette monnaie virtuelle (leaf.icon) mais aussi racheter ceux des autres joueurs. En échange d'une plus grosse somme d'argent, tu peux même bloquer un arbre pour que personne ne puisse le racheter et qu'il reste ta propriété !`}</p>
-        <p>{`Toutes les 15 minutes, tu reçevras le total de feuilles de tout tes arbres. Achetes en donc le plus possible pour recevoir encore plus de monnaie ! Par contre, toutes les 30 minutes tu perdras la moitié de tes feuilles... ( Winter is coming ! )`}</p>
-        <p>{`Voilà, tu sais tout ce qu'il y a à savoir pour commencer ton expension, tu peux commencer par acheter un nouvel arbre. N'hésite pas à consulter le Leaderboard ci-dessous pour consulter le classement et pourquoi pas tenter de faire partie du top 3 !`}</p>
+        <Scrollbars autoHeight autoHeightMin={500}>
+            <p>{`Welcome to Mwenbwa ${connectedUser.username} !`}</p>
+            <br />
+            <p>
+                {
+                    "Te voici voici à Liège, la cité ardente, et son étendue d'arbres. Tu viens de recevoir 3 arbres "
+                }
+                <TreeIcon />
+                {" ainsi qu'une certaine somme de feuilles "}
+                <LeafIcon />
+                {" pour commencer ton expension."}
+            </p>
+            <br />
+            <p>
+                {"Tu peux acheter des arbres libres "}
+                <TreeIcon />
+                {" avec cette monnaie virtuelle "}
+                <LeafIcon />
+                {
+                    " mais aussi racheter ceux des autres joueurs. En échange d'une plus grosse somme d'argent, tu peux même bloquer un arbre pour que personne ne puisse le racheter et qu'il reste ta propriété !"
+                }
+            </p>
+            <br />
+            <p>{`Toutes les 15 minutes, tu reçevras le total de feuilles de tout tes arbres. Achetes en donc le plus possible pour recevoir encore plus de monnaie ! Par contre, toutes les 30 minutes tu perdras la moitié de tes feuilles... ( Winter is coming ! )`}</p>
+            <br />
+            <p>{`Voilà, tu sais tout ce qu'il y a à savoir pour commencer ton expension, tu peux commencer par acheter un nouvel arbre. N'hésite pas à consulter le Leaderboard ci-dessous pour consulter le classement et pourquoi pas tenter de faire partie du top 3 !`}</p>
+        </Scrollbars>
     </div>
 );
 
