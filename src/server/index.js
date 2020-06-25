241225d9874590a0bc8197b614bb960141abaa81
thomasdussart <thomas.dussart@gmail.com>
2020-06-25 11:27:43 +0200

    aff arbres not working

--- src/server/index.js
+++ src/server/index.js
@@ -23,7 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const db = require("./models");
-const Role = db.role;
 
 const ConnectionMongoDb = require("./config/db.config");
 
