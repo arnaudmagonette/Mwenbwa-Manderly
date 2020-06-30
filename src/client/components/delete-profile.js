d8a2fc1435ae3ae5fdae3065dc76409b581f0e13
thomasdussart <thomas.dussart@gmail.com>
2020-06-30 09:51:25 +0200

    new

---
+++ src/client/components/delete-profile.js
@@ -0,0 +1,48 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable array-callback-return */
+import React from "react";
+import treeService from "../services/tree.service";
+import authService from "../services/auth.service";
+
+export default class DeleteProfile extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        treeService.getAllTrees().then(res => {
+            const userConnected = authService.getCurrentUser();
+            const userArray = [userConnected.username];
+            console.log(res.data);
+            console.log(Array.isArray(res.data));
+            for (let i = 0; i < res.data.length; i++) {
+                if (
+                    res.data[i].owner !== [] &&
+                    JSON.stringify(res.data[i].owner) ===
+                        JSON.stringify(userArray)
+                ) {
+                    //res.data[i].owner = [];
+                    console.log(res.data[i]._id);
+                }
+            }
+        });
+    }
+    render() {
+        return (
+            <div className={"columns  has-margin-top-10"}>
+                <div className={"column has-text-centered"}>
+                    <button
+                        name={"test"}
+                        onClick={this.handleClick}
+                        type={"submit"}
+                        className={
+                            "button is-primary  is-outlined has-margin-bottom-5 "
+                        }>
+                        {"Delete Profile"}
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
