22c786a3f393cb95be607211645a9b21ad4513ee
thomasdussart <thomas.dussart@gmail.com>
2020-07-02 17:28:52 +0200

    heroku

--- src/server/index.js
+++ src/server/index.js
@@ -44,13 +44,6 @@ app.listen(APP_PORT, () =>
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
 );
 
-//HEROKU
-// const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-// const server_host = process.env.YOUR_HOST || "0.0.0.0";
-// app.listen(server_port, server_host, () => {
-//     console.log("Listening on port %d", server_port);
-// });
-
 // Connection Mongo Db
 ConnectionMongoDb();
 
