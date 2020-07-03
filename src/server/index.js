81089c13faefab53bcc4751665e0686766cb9257
Arnaud Magonette <arnaud.magonette@gmail.com>
2020-07-03 14:35:09 +0200

    correction add log

--- src/server/index.js
+++ src/server/index.js
@@ -46,19 +46,19 @@ app.get("/*", (req, res) => {
 });
 
 //local
-app.listen(APP_PORT, () =>
-    console.log(`🚀 Server is listening on port ${APP_PORT}.`),
-);
+// app.listen(APP_PORT, () =>
+//     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
+// );
 
 // Connection Mongo Db
 ConnectionMongoDb();
 
 //heroku
-// const server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
-// const server_host = process.env.YOUR_HOST || "0.0.0.0";
-// app.listen(server_port, server_host, () => {
-//     console.log("Listening on port %d", server_port);
-// });
+const server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+const server_host = process.env.YOUR_HOST || "0.0.0.0";
+app.listen(server_port, server_host, () => {
+    console.log("Listening on port %d", server_port);
+});
 
 //------------------------------
 
